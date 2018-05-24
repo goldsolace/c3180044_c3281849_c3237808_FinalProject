8ebacb4b1bd6699beaef92088eb24e833ea36b3d
Jonathan Williams <c3237808@uon.edu.au>
2018-05-24 17:03:08 +1000

    Documented Bean classes

--- WEB-INF/classes/itserviceportal/model/beans/Comment.java
+++ WEB-INF/classes/itserviceportal/model/beans/Comment.java
@@ -13,14 +13,23 @@ import java.util.*;
 
 public class Comment implements Serializable {
 
-	private int commentID;
-	private String commentText;
-	private Date createdOn;
-	private User createdBy;
+	private int commentID;		//The ID of the comment
+	private String commentText;	//The comment made by the user
+	private Date createdOn;		//The date comment was made
+	private User createdBy;		//Which user made the comment
 
 	public Comment() {
 	}
 
+
+	/**
+	 * Overloaded Constructor
+	 *
+	 * @param commentID The id of the comment
+	 * @param commentText The comment being made
+	 * @param createdOn The date the comment was made
+	 * @param createdBy The user making the comment
+	 */
 	public Comment(int commentID, String commentText, Date createdOn, User createdBy) {
 		this.commentID = commentID;
 		this.commentText = commentText;
@@ -28,15 +37,31 @@ public class Comment implements Serializable {
 		this.createdBy = createdBy;
 	}
 
+
+	/**
+	 * commentID GETTER and SETTER
+	 */
 	public int getCommentID() { return commentID; }
 	public void setCommentID(int commentID) { this.commentID = commentID; }
 
-	public String getCommentText() { return commentText; }
-	public void setCommentText(String text) { this.commentText = commentText; }
 
+	/**
+	 * commentText GETTER and SETTER
+	 */
+	public String getCommentText() { return commentText; }
+	public void setCommentText(String text) { this.commentText = text; }
+
+
+	/**
+	 * createdOn GETTER and SETTER
+	 */
 	public Date getCreatedOn() { return createdOn; }
 	public void setCreatedOn(Date createdOn) { this.createdOn = createdOn; }
 
+
+	/**
+	 * createdBy GETTER and SETTER
+	 */
 	public User getCreatedBy() { return createdBy; }
 	public void setCreatedBy(User createdBy) { this.createdBy = createdBy; }
 }
